953f373c61ab05ca2d695374bc2b4318f544dfa0
dimijimz <dimijimz96@gmail.com>
2024-10-18 13:53:29 -0400

    Added functions to main

---
+++ src/main.py
@@ -0,0 +1,19 @@
+def main():
+    print("Welcome to Legend of Zelda: Echoes of the Lost Hero!")
+    while True:
+        command = input("What would you like to do? [scan, move, quit] ")
+        
+        if command == "quit":
+            print("Thanks for playing!")
+            break
+
+        elif command == "scan":
+            # Placeholder for the scan command
+            print("You activate the Echo Lens and scan the area.")
+
+        elif command == "move":
+            # Placeholder for the move command
+            print("Where would you like to move?")
+
+        else:
+            print("Invalid command. Please try again.")
